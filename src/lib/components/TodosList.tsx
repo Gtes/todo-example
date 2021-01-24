fa42871bb23071fe16eba5018ce24e720414c92f
Gtes <gates1993@gmail.com>
2021-01-24 19:00:32 +0200

    initial todos ui

--- src/lib/components/TodosList.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export interface ITodosListProps {}
-
-const TodosList: React.FC<ITodosListProps> = (props) => {
-    return <>test</>;
-};
-
-export default TodosList;
