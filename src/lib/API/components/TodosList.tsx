7e1dade2cf874c29f480f322aff4897729bcc707
Gtes <gates1993@gmail.com>
2021-01-24 17:54:07 +0200

    files setup

---
+++ src/lib/API/components/TodosList.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export interface ITodosListProps {}
+
+const TodosList: React.FC<ITodosListProps> = (props) => {
+    return <>test</>;
+};
+
+export default TodosList;
